514e8a0c3d3ff496bb214aa0eb3ac591050acc70
James Warner <jmswrnr@users.noreply.github.com>
2024-10-06 13:00:01 +0100

    feat: prevent multiple app instances

--- src/main/index.ts
+++ src/main/index.ts
@@ -20,13 +20,6 @@ let tray: Tray
 let spotifyWindow: BrowserWindow
 let settingsWindow: BrowserWindow
 
-applicationStore.subscribe(
-  (state) => state.isLoggedIn,
-  (isLoggedIn) => {
-    isLoggedIn ? spotifyWindow.hide() : spotifyWindow.show()
-  }
-)
-
 export const fetchImageFromRenderer = (url: string): Promise<Uint8Array> => {
   return new Promise((resolve, reject) => {
     ipcMain.once(`return-fetch-image-${url}`, (_event, data) => {
@@ -87,172 +80,185 @@ const sendStateToRenderer = (slice: RemoteApplicationState) => {
   settingsWindow.webContents.send('state-update', slice)
 }
 
-applicationStore.subscribe(remoteStateSlice, (slice) => sendStateToRenderer(slice), {
-  equalityFn: shallow
-})
+const gotTheLock = app.requestSingleInstanceLock()
 
-app.whenReady().then(async () => {
-  try {
-    initFiles()
-  } catch {
-    dialog.showErrorBox('Spotilocal Initialization Error', 'Error creating output files')
-  }
-  try {
-    await components.whenReady([components.WIDEVINE_CDM_ID])
-  } catch {
-    dialog.showErrorBox(
-      'Spotilocal Initialization Error',
-      'Unable to load required Widevine CDM components'
-    )
-    return app.exit(1)
-  }
-
-  getIsUpdateAvailable().then((isUpdateAvailable: boolean) => {
-    applicationStore.setState({
-      isUpdateAvailable
-    })
-  })
-
-  settingsWindow = new BrowserWindow({
-    ...SETTINGS_WINDOW_SIZE,
-    useContentSize: true,
-    alwaysOnTop: true,
-    fullscreenable: false,
-    frame: false,
-    resizable: false,
-    show: false,
-    skipTaskbar: true,
-    transparent: true,
-    webPreferences: {
-      sandbox: false,
-      devTools: true,
-      preload: join(__dirname, '../preload/settings.js')
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.whenReady().then(async () => {
+    try {
+      initFiles()
+    } catch {
+      dialog.showErrorBox('Spotilocal Initialization Error', 'Error creating output files')
     }
-  })
-  settingsWindow.setMenu(null)
-  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    settingsWindow.loadURL(process.env['ELECTRON_RENDERER_URL'] + '/preferences/index.html')
-  } else {
-    settingsWindow.loadFile(join(__dirname, '../renderer/preferences/index.html'))
-  }
-  settingsWindow.on('resize', () => {
-    positionWindowToTray(settingsWindow)
-  })
-
-  if (!is.dev) {
-    settingsWindow.on('blur', () => {
-      settingsWindow.hide()
-    })
-  }
-
-  tray = new Tray(icon)
-  tray.on('click', () => toggleSettingsWindow())
-  tray.on('right-click', () => toggleSettingsWindow())
-  tray.setTitle('Spotilocal')
-  tray.setToolTip('Spotilocal')
-
-  spotifyWindow = new BrowserWindow({
-    width: 1280,
-    height: 800,
-    show: false,
-    webPreferences: {
-      sandbox: false,
-      partition: 'persist:spotify',
-      contextIsolation: false,
-      preload: join(__dirname, '../preload/spotify.js')
+    try {
+      await components.whenReady([components.WIDEVINE_CDM_ID])
+    } catch {
+      dialog.showErrorBox(
+        'Spotilocal Initialization Error',
+        'Unable to load required Widevine CDM components'
+      )
+      return app.exit(1)
     }
-  })
-  spotifyWindow.setMenu(null)
-  spotifyWindow.webContents.setUserAgent(
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
-  )
-  spotifyWindow.webContents.session.webRequest.onHeadersReceived((details, callback) => {
-    if (details.url === __LOGIN_URL__) {
+
+    getIsUpdateAvailable().then((isUpdateAvailable: boolean) => {
       applicationStore.setState({
-        isLoggedIn: details.statusCode === 302
+        isUpdateAvailable
+      })
+    })
+
+    settingsWindow = new BrowserWindow({
+      ...SETTINGS_WINDOW_SIZE,
+      useContentSize: true,
+      alwaysOnTop: true,
+      fullscreenable: false,
+      frame: false,
+      resizable: false,
+      show: false,
+      skipTaskbar: true,
+      transparent: true,
+      webPreferences: {
+        sandbox: false,
+        devTools: true,
+        preload: join(__dirname, '../preload/settings.js')
+      }
+    })
+    settingsWindow.setMenu(null)
+    if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+      settingsWindow.loadURL(process.env['ELECTRON_RENDERER_URL'] + '/preferences/index.html')
+    } else {
+      settingsWindow.loadFile(join(__dirname, '../renderer/preferences/index.html'))
+    }
+    settingsWindow.on('resize', () => {
+      positionWindowToTray(settingsWindow)
+    })
+
+    if (!is.dev) {
+      settingsWindow.on('blur', () => {
+        settingsWindow.hide()
       })
     }
-    callback({ cancel: false, responseHeaders: details.responseHeaders })
-  })
 
-  spotifyWindow.loadURL(__LOGIN_URL__)
+    tray = new Tray(icon)
+    tray.on('click', () => toggleSettingsWindow())
+    tray.on('right-click', () => toggleSettingsWindow())
+    tray.setTitle('Spotilocal')
+    tray.setToolTip('Spotilocal')
 
-  spotifyWindow.on('close', (event) => {
-    event.preventDefault()
-    spotifyWindow.hide()
-  })
-
-  electronApp.setAppUserModelId('com.spotilocal.app')
-
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
-
-  ipcMain.on('set-user-settings', (_event, patch: Partial<UserSettings>) => {
-    applicationStore.setState(
-      produce<ApplicationState>((state) => {
-        state.userSettings = {
-          ...state.userSettings,
-          ...patch
-        }
-      })
+    spotifyWindow = new BrowserWindow({
+      width: 1280,
+      height: 800,
+      show: false,
+      webPreferences: {
+        sandbox: false,
+        partition: 'persist:spotify',
+        contextIsolation: false,
+        preload: join(__dirname, '../preload/spotify.js')
+      }
+    })
+    spotifyWindow.setMenu(null)
+    spotifyWindow.webContents.setUserAgent(
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
     )
-  })
+    spotifyWindow.webContents.session.webRequest.onHeadersReceived((details, callback) => {
+      if (details.url === __LOGIN_URL__) {
+        applicationStore.setState({
+          isLoggedIn: details.statusCode === 302
+        })
+      }
+      callback({ cancel: false, responseHeaders: details.responseHeaders })
+    })
 
-  ipcMain.on('get-state', () => {
-    sendStateToRenderer(remoteStateSlice(applicationStore.getState()))
-  })
+    spotifyWindow.loadURL(__LOGIN_URL__)
 
-  ipcMain.on('get-update', () => {
-    shell.openPath('https://github.com/jmswrnr/spotilocal/releases/latest')
-    settingsWindow.blur()
-  })
+    spotifyWindow.on('close', (event) => {
+      event.preventDefault()
+      spotifyWindow.hide()
+    })
 
-  ipcMain.on('quit', () => {
-    spotifyWindow.destroy()
-    settingsWindow.destroy()
-    app.quit()
-  })
+    applicationStore.subscribe(
+      (state) => state.isLoggedIn,
+      (isLoggedIn) => {
+        isLoggedIn ? spotifyWindow.hide() : spotifyWindow.show()
+      }
+    )
 
-  ipcMain.on('logout', async () => {
-    await spotifyWindow?.webContents.session.clearStorageData()
-    await spotifyWindow?.loadURL(__LOGIN_URL__)
-    spotifyWindow?.moveTop()
-    spotifyWindow?.focus()
-    spotifyWindow?.show()
-    settingsWindow.blur()
-  })
+    applicationStore.subscribe(remoteStateSlice, (slice) => sendStateToRenderer(slice), {
+      equalityFn: shallow
+    })
 
-  ipcMain.on('login', async () => {
-    await spotifyWindow?.loadURL(__LOGIN_URL__)
-    spotifyWindow?.moveTop()
-    spotifyWindow?.focus()
-    spotifyWindow?.show()
-  })
+    electronApp.setAppUserModelId('com.spotilocal.app')
 
-  ipcMain.on('spotify-logged-in', () => {
-    applicationStore.setState({
-      isLoggedIn: true
+    app.on('browser-window-created', (_, window) => {
+      optimizer.watchWindowShortcuts(window)
+    })
+
+    ipcMain.on('set-user-settings', (_event, patch: Partial<UserSettings>) => {
+      applicationStore.setState(
+        produce<ApplicationState>((state) => {
+          state.userSettings = {
+            ...state.userSettings,
+            ...patch
+          }
+        })
+      )
+    })
+
+    ipcMain.on('get-state', () => {
+      sendStateToRenderer(remoteStateSlice(applicationStore.getState()))
+    })
+
+    ipcMain.on('get-update', () => {
+      shell.openPath('https://github.com/jmswrnr/spotilocal/releases/latest')
+      settingsWindow.blur()
+    })
+
+    ipcMain.on('quit', () => {
+      spotifyWindow.destroy()
+      settingsWindow.destroy()
+      app.quit()
+    })
+
+    ipcMain.on('logout', async () => {
+      await spotifyWindow?.webContents.session.clearStorageData()
+      await spotifyWindow?.loadURL(__LOGIN_URL__)
+      spotifyWindow?.moveTop()
+      spotifyWindow?.focus()
+      spotifyWindow?.show()
+      settingsWindow.blur()
+    })
+
+    ipcMain.on('login', async () => {
+      await spotifyWindow?.loadURL(__LOGIN_URL__)
+      spotifyWindow?.moveTop()
+      spotifyWindow?.focus()
+      spotifyWindow?.show()
+    })
+
+    ipcMain.on('spotify-logged-in', () => {
+      applicationStore.setState({
+        isLoggedIn: true
+      })
+    })
+
+    ipcMain.on('spotify-track-data', (_event, tracks) => handleSpotifyTrackData(tracks))
+
+    ipcMain.on('spotify-player-state', (_event, player_state) =>
+      handleSpotifyPlayerState(player_state)
+    )
+
+    ipcMain.on('set-system-clock-offset', (_event, drift) => {
+      const systemClockOffset = Number(drift)
+      if (isNaN(systemClockOffset)) {
+        return
+      }
+      applicationStore.setState({
+        systemClockOffset: systemClockOffset
+      })
+    })
+
+    app.on('window-all-closed', () => {
+      app.quit()
     })
   })
-
-  ipcMain.on('spotify-track-data', (_event, tracks) => handleSpotifyTrackData(tracks))
-
-  ipcMain.on('spotify-player-state', (_event, player_state) =>
-    handleSpotifyPlayerState(player_state)
-  )
-
-  ipcMain.on('set-system-clock-offset', (_event, drift) => {
-    const systemClockOffset = Number(drift)
-    if (isNaN(systemClockOffset)) {
-      return
-    }
-    applicationStore.setState({
-      systemClockOffset: systemClockOffset
-    })
-  })
-
-  app.on('window-all-closed', () => {
-    app.quit()
-  })
-})
+}
