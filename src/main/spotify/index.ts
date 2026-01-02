8fc3267482fccd92986d2c8c174ffcfed73cc780
James Warner <jmswrnr@users.noreply.github.com>
2026-01-02 16:58:14 +0000

    feat: ensure app data folder exists

--- src/main/spotify/index.ts
+++ src/main/spotify/index.ts
@@ -7,11 +7,15 @@ import './file-exports/images'
 import './file-exports/json'
 import './file-exports/history'
 
+import { mkdirSync } from 'node:fs'
 import { initializeDefaultImageFiles } from './file-exports/images'
 import { initializeTrackFiles, writeBlankTrackToDisk } from './file-exports/track'
 import { writeBlankAlbumToDisk } from './file-exports/album'
+import { outputDirectory } from '../constants'
 
 export const initFiles = () => {
+  mkdirSync(outputDirectory, { recursive: true })
+  
   initializeDefaultImageFiles()
   initializeTrackFiles()
   writeBlankTrackToDisk()
